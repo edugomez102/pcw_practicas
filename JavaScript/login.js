48b5453f77395d9ed80417316730129bb6f9a8bf
deivid <videdavid@hotmail.com>
2020-03-23 19:24:22 +0100

    Login hecho funcionando de PUTA MADRE

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,1 +1,36 @@
- 
+function hacerLogin(formu){
+	let xhr = new XMLHttpRequest(),
+		url = 'api/usuarios/login',
+		fr	= new FormData(formu);
+	let wModal = document.querySelector(".modal");
+	let buttonAceptar = document.getElementById('AceptarLogin');
+
+	xhr.open('POST',url,true);
+
+	xhr.onload = function(){
+		let objJava = JSON.parse(xhr.responseText);
+		let mensajeModal = "";
+		console.log(objJava);
+		if (objJava.RESULTADO == "OK") {
+			mensajeModal = `<p>Usuario ${objJava.login} identificado correctamente</p>`;
+			sessionStorage['usuario'] = JSON.stringify(objJava);
+			wModal.style.display = 'block';
+			document.querySelector('#mensajeLogin').innerHTML = mensajeModal;
+			buttonAceptar.addEventListener('click',function(){
+				window.location = "/pcw_practicas/index.html";
+			});
+		}else{
+			mensajeModal = `<p>Usuario/contraseña incorrectos</p>`;
+			wModal.style.display = 'block';
+			document.querySelector('#mensajeLogin').innerHTML = mensajeModal;
+			buttonAceptar.addEventListener('click',function(){
+				wModal.style.display = 'none';
+			});
+		}
+	};
+
+	xhr.send(fr);
+	return false;
+
+}
+
